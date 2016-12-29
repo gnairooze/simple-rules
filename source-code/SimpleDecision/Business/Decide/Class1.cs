f1c9fe9ed2dac24315d5ed68ceaa52ff6e9ded40
gnairooze <gnairooze@gmail.com>
2016-12-29 19:03:05 +0200

    add solution structure [business, model, unit test]

---
+++ source-code/SimpleDecision/Business/Decide/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDecision.Business.Decide
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
